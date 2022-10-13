cff980e46e1ae979decd6fa44c6f47d5867234a2
Robertas Misevičius <robertas31810@gmail.com>
2022-10-13 14:04:11 +0300

    4, 5, 6, 7 užduotys

--- scripts/for.js
+++ scripts/for.js
@@ -29,26 +29,50 @@ console.log('for.js');
 
 // 4. atspausdinti 15 random verciu nuo 1 iki 10
 // 4.1 atspausdinti verciu vidurki
-let sum = 0;
-for (let i = 0; i <= 15; i++ ){
+
+// let sum = 0;
+// for (let i = 0; i <= 15; i++ ){
     
-    let random = Math.trunc((Math.random() * 10) + 1 );
-    sum = sum + random;
+//     let random = Math.trunc((Math.random() * 10) + 1 );
+//     sum = sum + random;
     
 
-}
-console.log(sum);
-console.log(sum / 15);
+// }
+// console.log(sum);
+// console.log(sum / 15);
 
 
 // 5. atspausdinti nuo 47 iki 68 visus lyginius skaicius
 
-
+// for (let i = 47; i <= 68; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
 
 
 // 6. sudeti visus skaicius nuo 0 iki 10, atspusdinti rezultata
 
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+    
+//     sum = sum + i;
+    
+// }
+// console.log(sum);
 
 // 7. su while loop. parasyti cikla kuris meta kauliukus ir galimos reiksmes yra nuo 2 iki 12.
 // 7.1 kiek kartu kompiuteris mes kauliukus kol ismes 12?
-// 7.2 kiek kartu kompiuteris mes kauliukus kol daugiau nei 6?
+
+
+let i = 2 ;
+let a = 0;
+let b = 0;
+while (i  !== 12){
+    i = Math.trunc((Math.random() * 12) + 1 );
+    a++;
+   
+}
+ console.log(a);
+
+ // 7.2 kiek kartu kompiuteris mes kauliukus kol daugiau nei 6?
