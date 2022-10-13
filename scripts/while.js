4cc3fbe6321cd09b91ee0e0cb704a989570d2dd2
Robertas Misevičius <robertas31810@gmail.com>
2022-10-13 08:43:10 +0300

    Pradinis

---
+++ scripts/while.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('./scripts/while.js');
+let vardas  = 'Mozart';
+
+console.log('Vardas', vardas);
+
+const jaison = {
+    vardas: 'Miau',
+    vidinisObjektas: {
+        age: 15,
+    },
+    kitasVidinisObjektas: {
+        age:15,
+    },
+}
